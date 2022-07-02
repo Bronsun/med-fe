d968d4afa1ab0018c01acc76ecda2eb0b1f4cc20
Bronsun <mateusz.broncel1998@gmail.com>
2022-07-02 21:17:25 +0200

    Initi

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
 }
+const withSass = require("@zeit/next-sass");
+module.exports = withSass();
+
+module.exports = withSass({
+  target: 'serverless',
+  env: {
+    JWT_SECRET: process.env.JWT_SECRET
+  }
+});
 
 module.exports = nextConfig
